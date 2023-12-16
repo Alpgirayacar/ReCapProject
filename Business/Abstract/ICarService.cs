68ca3aaef9844d398d302d143b2af071f4ca6c6e
alpgiray <alpgirayacar@outlook.com>
2023-12-16 15:09:38 +0300

    CarImage Service, Manager and Controller added

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -11,14 +11,14 @@ namespace Business.Abstract
 {
     public interface ICarService
     {
-      IDataResult<List<Car>> GetAll();
+        IDataResult<List<Car>> GetAll();
 
-       IDataResult <Car> GetById(int id);
+        IDataResult <Car> GetById(int id);
         IDataResult<List<Car>> GetAllByBrandId(int brandId);
 
         IDataResult<List<CarDetailDto>> GetCarDetails();
 
-      IResult Add(Car car);
+        IResult Add(Car car);
 
         IResult   Update (Car car);   
         IResult Delete (Car car);   
