35469fd493c7907690db6280ae2584e775cb5339
alpgiray <alpgirayacar@outlook.com>
2023-12-16 21:17:46 +0300

    Autofac operations added

--- Business/DependencyResolves/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolves/Autofac/AutofacBusinessModule.cs
@@ -4,6 +4,7 @@ using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
+using Core.Utilities.Security.Jwt;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -28,6 +29,11 @@ namespace Business.DependencyResolves.Autofac
             builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
 
 
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
          
 
