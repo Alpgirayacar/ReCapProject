45e7794f149ccecbacff76f1447bdcee1c3ed6c8
alpgiray <alpgirayacar@outlook.com>
2023-12-16 21:20:50 +0300

    EntityFramework Data Acces Classes edited

--- DataAccess/Abstract/IUserDal.cs
+++ DataAccess/Abstract/IUserDal.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities.Concrete;
 using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
     public interface IUserDal:IEntityRepository<User>
     {
-
+        List<OperationClaim> GetClaims(User user);
     }
 }
