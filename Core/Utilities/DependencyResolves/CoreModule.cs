1aa88a85777b99c0ece96cd0a57060728eaeeb49
alpgiray <alpgirayacar@outlook.com>
2023-12-19 18:20:03 +0300

    Core module Added

---
+++ Core/Utilities/DependencyResolves/CoreModule.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utilities.IoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DependencyResolves;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+
+namespace Core.DependencyResolves
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMemoryCache();
+           serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager,MemoryCacheManaer>();
+        }
+    }
+}
